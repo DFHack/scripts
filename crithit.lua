0aa1f3a9688c019ee90c78df37f2344ac7b88c00
maxthyme <simply.relativity@gmail.com>
2017-06-21 22:42:05 -0500

    Remembered a new trick I learned.
    Cleans it up a bit more.

--- crithit.lua
+++ crithit.lua
@@ -3,23 +3,13 @@ local unit = df.global.world.units.active[0]
 local attks = unit.actions
 for k,v in ipairs(attks) do
 	if attks[k].type==1 then
-		unit.body.body_plan.attacks[0].velocity_modifier=2700000
-		unit.body.body_plan.attacks[0].contact_perc=100000
-		unit.body.body_plan.attacks[0].penetration_perc=100000
-		unit.body.body_plan.attacks[0].flags.edge=true
-		unit.body.body_plan.attacks[1].velocity_modifier=2700000
-		unit.body.body_plan.attacks[1].contact_perc=100000
-		unit.body.body_plan.attacks[1].penetration_perc=100000
-		unit.body.body_plan.attacks[1].flags.edge=true
-		unit.body.body_plan.attacks[2].velocity_modifier=2700000
-		unit.body.body_plan.attacks[2].contact_perc=100000
-		unit.body.body_plan.attacks[2].penetration_perc=100000
-		unit.body.body_plan.attacks[2].flags.edge=true
-		unit.body.body_plan.attacks[3].velocity_modifier=2700000
-		unit.body.body_plan.attacks[3].contact_perc=100000
-		unit.body.body_plan.attacks[3].penetration_perc=100000
-		unit.body.body_plan.attacks[3].flags.edge=true
 		attks[k].data.attack.attack_accuracy=1000
 		attks[k].data.attack.attack_velocity=9999999
+	end
+	for i = 0,3 do
+		unit.body.body_plan.attacks[i].velocity_modifier=2700000
+		unit.body.body_plan.attacks[i].contact_perc=100000
+		unit.body.body_plan.attacks[i].penetration_perc=100000
+		unit.body.body_plan.attacks[i].flags.edge=true
 	end
 end
