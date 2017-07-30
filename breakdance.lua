a6893edbd6eebb041debe8056704a311b6484c01
maxthyme <simply.relativity@gmail.com>
2017-07-30 11:26:32 +0000

    Update breakdance.lua

--- breakdance.lua
+++ breakdance.lua
@@ -1,4 +1,11 @@
 --Breaks up a stuck dance activity.
+--[====[
+
+breakdance
+=====
+Breaks up broken or otherwise hung dances that occur when a unit can't find a partner.
+
+]====]
 local unit = df.global.world.units.active[0]
 local act = unit.social_activities[0]
 if df.activity_entry.find(act).type==8 then
