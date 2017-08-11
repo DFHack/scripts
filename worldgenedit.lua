e8adbb9c2bb388843d0bee62bf5c6d988272d935
maxthyme <simply.relativity@gmail.com>
2017-08-11 23:24:52 +0000

    Added race/total pop count display.

--- worldgenedit.lua
+++ worldgenedit.lua
@@ -13,7 +13,9 @@ local widgets = require 'gui.widgets'
 local wv = dfhack.gui.getCurViewscreen()
 local wd = df.global.world.world_data
 local wg = df.global.world.worldgen.worldgen_parms
-local stn, stc, pop, eny
+local sites = df.global.world.world_data.sites
+local ents = df.global.world.entities.all
+local pent, dent, dnm, eent, enm, wnm, hent, hnm, gent, gnm, stn, stc, pop, eny
 
 genedit=defclass(genedit,gui.Screen)
 genedit.focus_path = 'worldgenedit'
@@ -27,12 +29,17 @@ function genedit:init()
     self:addviews{
       widgets.Label{
         view_id="main",
-        frame = {t=18,l=0},
+        frame = {t=18,l=0}, 
         text={
             {text="Quit: Enter/Esc        "},{id="prec", text="Site Num: "}, {id="curs", text=self:callback("getSites")},NEWLINE,
             {text="Edit: Left/Right(Fast) "},{id="pres", text="Site Cap: "}, {id="caps", text=self:callback("getCaps")},NEWLINE,
             {text="Edit: Up/Down(Fast/Z)  "},{id="ends", text="End Year: "}, {id="endy", text=self:callback("getEny")},NEWLINE,
-            {text="Edit: PgUp/Dn(Fast)    "},{id="pops", text="Pop Cap: "}, {id="popc", text=self:callback("getPops")},
+            {text="Edit: PgUp/Dn(Fast)    "},{id="pops", text="Pop Cap: "}, {id="popc", text=self:callback("getPops")},NEWLINE,
+            {text="Dwarves:       "},{id="drf", text=self:callback("getDwf")},NEWLINE,
+            {text="Humans:        "},{id="hmn", text=self:callback("getHum")},NEWLINE,
+            {text="Elves:         "},{id="elf", text=self:callback("getElf")},NEWLINE,
+            {text="Goblins:       "},{id="grb", text=self:callback("getGob")},NEWLINE,
+            {text="Total:         "},{id="pll", text=self:callback("getAll")},
             }
         }
     }
@@ -58,13 +65,70 @@ function genedit:getEny()
     return eny
 end
 
+function genedit:getDwf()
+    dent = 0
+    for k = 0, #sites-1, 1 do
+        local inh = sites[k].inhabitants
+        for v = 0, #inh-1, 1 do
+            if df.creature_raw.find(inh[v].race).creature_id=='DWARF' then
+                dent = dent + inh[v].count
+            end
+        end
+    end 
+    return dent
+end
+
+function genedit:getHum()
+    hent = 0
+    for k = 0, #sites-1, 1 do
+        local inh = sites[k].inhabitants
+        for v = 0, #inh-1, 1 do
+            if df.creature_raw.find(inh[v].race).creature_id=='HUMAN' then
+                hent = hent + inh[v].count
+            end
+        end
+    end 
+    return hent
+end
+
+function genedit:getElf()
+    eent = 0
+    for k = 0, #sites-1, 1 do
+        local inh = sites[k].inhabitants
+        for v = 0, #inh-1, 1 do
+            if df.creature_raw.find(inh[v].race).creature_id=='ELF' then
+                eent = eent + inh[v].count
+            end
+        end
+    end 
+    return eent
+end
+
+function genedit:getGob()
+    gent = 0
+    for k = 0, #sites-1, 1 do
+        local inh = sites[k].inhabitants
+        for v = 0, #inh-1, 1 do
+            if df.creature_raw.find(inh[v].race).creature_id=='GOBLIN' then
+                gent = gent + inh[v].count
+            end
+        end
+    end 
+    return gent
+end
+
+function genedit:getAll()
+    pent = dent+eent+hent+gent
+    return pent
+end
 
 function genedit:onInput(keys)
     if df.viewscreen_new_regionst:is_instance(wv) then
         if keys.LEAVESCREEN or keys.SELECT then
                    self:dismiss()
+                   wv.worldgen_paused = 0
         end
-        if keys.CURSOR_LEFT then
+        if keys.CURSOR_LEFT then 
             wg.site_cap = wg.site_cap-25
         elseif keys.CURSOR_RIGHT then
             wg.site_cap = wg.site_cap+25
