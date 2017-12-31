09d105ebc75b278bde3971754eccb30fdd937489
rndmvar <rndmvar@gmail.com>
2017-12-31 16:46:00 -0600

    Bugfix reaction-product-trigger
    The external function call getWorkerAndBuilding fails in Adventure mode when crafting outside of a building.  Requiring that unit.job.current_job not be 'nil' before calling the external function works around this issue.  These checks are carried out in 'modtools/reaction-trigger.lua' before calling the internal function.  So, changing reaction-product-trigger.lua, to follow the methodology present in reaction-trigger.lua, probably makes the most sense.

--- modtools/reaction-product-trigger.lua
+++ modtools/reaction-product-trigger.lua
@@ -79,7 +79,11 @@ end
 
 local function afterProduce(reaction,reaction_product,unit,input_items,input_reagents,output_items)
  --printall(unit.job.current_job)
- local _,buildingId = dfhack.script_environment('modtools/reaction-trigger').getWorkerAndBuilding(unit.job.current_job)
+ local _,buildingId = -1,-1
+ -- unit.job.current_job might be nil for Adventure mode
+ if unit.job.current_job then
+  _,buildingId = dfhack.script_environment('modtools/reaction-trigger').getWorkerAndBuilding(unit.job.current_job)
+ end
  for _,hook in ipairs(productHooks[reaction.code] or {}) do
   local command = hook.command
   local processed = processArgs(command, reaction, reaction_product, unit, input_items, input_reagents, output_items, buildingId)
