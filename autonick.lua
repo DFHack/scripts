1c966935ba8e9517a5aab061848bfd2e08d83137
Su <moth.tolias@gmail.com>
2022-03-29 01:06:44 +0100

    reduce indentation

--- autonick.lua
+++ autonick.lua
@@ -44,57 +44,58 @@ if #commands ~= 0 then options.help = true end
 
 if options.help == true then
     print(dfhack.script_help())
-else
-    result = options.force;
-    if options.force ~= true then
-        local utils = require("utils")
-        result = utils.prompt_yes_no("nickname unnicked dwarves? (use -f to disable this check)", true)
+    return
+end
+
+result = options.force;
+if options.force ~= true then
+    local utils = require("utils")
+    result = utils.prompt_yes_no("nickname unnicked dwarves? (use -f to disable this check)", true)
+end
+
+if result == true then
+    local seen = {}
+    --check current nicknames
+    for _,unit in ipairs(df.global.world.units.active) do
+        if dfhack.units.isCitizen(unit) and
+        unit.name.nickname ~= "" then
+            seen[unit.name.nickname] = true
+        end
     end
 
-    if result == true then
-        local seen = {}
-        --check current nicknames
-        for _,unit in ipairs(df.global.world.units.active) do
-            if dfhack.units.isCitizen(unit) and
-            unit.name.nickname ~= "" then
-                seen[unit.name.nickname] = true
-            end
+    local names = {}
+    -- grab list, put in array
+    local path = dfhack.getDFPath () .. "/dfhack-config/autonick.txt";
+    for line in io.lines(path) do
+        line = line:trim()
+        if (line ~= "")
+        and (not line:startswith('#'))
+        and (not seen[line]) then
+            table.insert(names, line)
+            seen[line] = true
         end
+    end
 
-        local names = {}
-        -- grab list, put in array
-        local path = dfhack.getDFPath () .. "/dfhack-config/autonick.txt";
-        for line in io.lines(path) do
-            line = line:trim()
-            if (line ~= "")
-            and (not line:startswith('#'))
-            and (not seen[line]) then
-                table.insert(names, line)
-                seen[line] = true
-            end
+    --assign names
+    count = 0
+    for _,unit in ipairs(df.global.world.units.active) do
+        if (#names == 0) then
+                if options.quiet ~= true then
+                    print("no free names left in autonick.txt.")
+                end
+            break
         end
+        --if there are any names left
+        if dfhack.units.isCitizen(unit) and
+        unit.name.nickname == "" then
+            newnameIndex = math.random (#names)
+            dfhack.units.setNickname(unit, names[newnameIndex])
+            table.remove(names, newnameIndex)
+            count = count + 1
+        end
+    end
 
-        --assign names
-        count = 0
-        for _,unit in ipairs(df.global.world.units.active) do
-            if (#names == 0) then
-                    if options.quiet ~= true then
-                        print("no free names left in autonick.txt.")
-                    end
-                break
-            end
-            --if there are any names left
-            if dfhack.units.isCitizen(unit) and
-            unit.name.nickname == "" then
-                newnameIndex = math.random (#names)
-                dfhack.units.setNickname(unit, names[newnameIndex])
-                table.remove(names, newnameIndex)
-                count = count + 1
-            end
-        end
-
-        if options.quiet ~= true then
-            print(("gave nicknames to %s dwarves."):format(count))
-        end
+    if options.quiet ~= true then
+        print(("gave nicknames to %s dwarves."):format(count))
     end
 end
