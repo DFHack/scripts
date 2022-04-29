2e508e50b8a10ada83543e10047273b6838ce7bf
Myk <myk002@yahoo.com>
2022-04-29 09:10:41 -0700

    Fix formatting in docstring

--- modtools/fire-rate.lua
+++ modtools/fire-rate.lua
@@ -30,49 +30,49 @@ Requires a Target and any number of Modifiers.
 
 Targets:
 
-:--item <item token>:
+:-item <item token>:
   The full token of the item to modify.
   Example: ``WEAPON:ITEM_WEAPON_BOW``
-:--throw:
+:-throw:
   Modify the fire rate for throwing.
   This is specifically for thrown attacks without a weapon - if you have a
-  weapon that uses ``THROW`` as its skill, you need to use the ``--item``
+  weapon that uses ``THROW`` as its skill, you need to use the ``-item``
   argument for it.
-:--material <material token>:
-  Apply only to items made of the given material token. With the ``--item``
+:-material <material token>:
+  Apply only to items made of the given material token. With the ``-item``
   argument, this will apply to the material that the weapon is made of, whereas
-  with the ``--throw`` argument this will apply to the material being thrown (or
+  with the ``-throw`` argument this will apply to the material being thrown (or
   fired, in the case of interactions). This is optional.
   Format examples: "CREATURE:COW:MILK", "PLANT:MUSHROOM_HELMET_PLUMP:DRINK",
   "INORGANIC:GOLD", "VOMIT"
 
 Modifiers:
 
-:--fortBase <integer> --advBase <integer>:
+:-fortBase <integer> -advBase <integer>:
   Set the base fire rate for the weapon in ticks to use in the respective mode
   (fortress/adventure). Means one shot per x ticks. Defaults to the game default
   of 80.
-:--fortSkillFactor <float> --advSkillFactor <float>:
+:-fortSkillFactor <float> -advSkillFactor <float>:
   Multiplier that modifies how effective a user's skill is at improving the fire
   rate in the respective modes. In basic mode, recovery time is reduced by this
   value * user's skill ticks. Defaults to 2.7. With that value and default
   settings, it will make a Legendary shooter fire at the speed cap.
-:--fortCap <integer> --advCap <integer>:
+:-fortCap <integer> -advCap <integer>:
   Sets a cap on the fastest fire rate that can be achieved in their respective
   mode. Due to game limitations, the cap can't be less than 10 in adventure
-  mode. Defaults to half of the base fire rate defined by the ``--fort`` or
-  ``--adv`` arguments.
+  mode. Defaults to half of the base fire rate defined by the ``-fort`` or
+  ``-adv`` arguments.
 
 Other:
 
-:--mode <"basic" | "vanilla">:
-  | Sets what method is used to determine how skill affects fire rates. This is
-  | applied globally, rather than on a per-item basis. Basic uses a simplified
-  | method for working out fire rates - each point in a skill reduces the fire
-  | cooldown by a consistent, fixed amount. This method is the default.
-  | Vanilla mode attempts to replicate behaviour for fire rates - skill rolls
-  | determine which of 6 fixed increments of speeds is used, with a unit's skill
-  | affecting the range and averages. **NOT YET IMPLEMENTED!**
+:-mode <"basic" | "vanilla">:
+  Sets what method is used to determine how skill affects fire rates. This is
+  applied globally, rather than on a per-item basis. Basic uses a simplified
+  method for working out fire rates - each point in a skill reduces the fire
+  cooldown by a consistent, fixed amount. This method is the default.
+  Vanilla mode attempts to replicate behaviour for fire rates - skill rolls
+  determine which of 6 fixed increments of speeds is used, with a unit's skill
+  affecting the range and averages. **NOT YET IMPLEMENTED!**
 
 ]====]
 
