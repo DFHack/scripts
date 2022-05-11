e5a6401654644b4dc6c7e01b081ce1a123988f9a
lethosor <lethosor@gmail.com>
2022-05-11 00:53:55 -0400

    Check cases where getAddress() returns null

--- test/startdwarf.lua
+++ test/startdwarf.lua
@@ -1,11 +1,16 @@
 local utils = require('utils')
 
-local function with_patches(callback)
+local function with_patches(callback, custom_mocks)
     dfhack.with_temp_object(df.new('uint32_t'), function(temp_out)
         local mocks = {
             getAddress = mock.func(utils.addressof(temp_out)),
             patchMemory = mock.func(),
         }
+        if custom_mocks then
+            for k, v in pairs(custom_mocks) do
+                mocks[k] = v
+            end
+        end
         mock.patch({
             {dfhack.internal, 'getAddress', mocks.getAddress},
             {dfhack.internal, 'patchMemory', mocks.patchMemory},
@@ -19,7 +24,7 @@ local function run_startdwarf(...)
     return dfhack.run_script('startdwarf', ...)
 end
 
-local function test_invalid_args(args, expected_message)
+local function test_early_error(args, expected_message, custom_mocks)
     with_patches(function(mocks, temp_out)
         temp_out.value = 12345
 
@@ -32,8 +37,13 @@ local function test_invalid_args(args, expected_message)
 
         expect.eq(mocks.patchMemory.call_count, 0, 'patchMemory was called unexpectedly')
 
+        -- make sure the script didn't attempt to write in some other way
         expect.eq(temp_out.value, 12345, 'memory was changed unexpectedly')
-    end)
+    end, custom_mocks)
+end
+
+local function test_invalid_args(args, expected_message)
+    test_early_error(args, expected_message)
 end
 
 local function test_patch_successful(expected_value)
@@ -65,6 +75,11 @@ function test.too_small()
     test_invalid_args({'4'}, 'too small')
     test_invalid_args({'6'}, 'too small')
     test_invalid_args({'-1'}, 'too small')
+end
+
+function test.missing_address()
+    test_early_error({}, 'address not available', {getAddress = mock.func(nil)})
+    test_early_error({'8'}, 'address not available', {getAddress = mock.func(nil)})
 end
 
 function test.exactly_7()
