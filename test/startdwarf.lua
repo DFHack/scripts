85dc5d3bf387667e2b8052e6d389466a980c6eb9
lethosor <lethosor@gmail.com>
2022-05-27 01:06:54 -0400

    Use mock.observe_func() to verify that patchMemory() has the intended effect
    Addresses a gap brought up in code review

--- test/startdwarf.lua
+++ test/startdwarf.lua
@@ -2,9 +2,18 @@ local utils = require('utils')
 
 local function with_patches(callback, custom_mocks)
     dfhack.with_temp_object(df.new('uint32_t'), function(temp_out)
+        local originalPatchMemory = dfhack.internal.patchMemory
+        local function safePatchMemory(target, source, length)
+            -- only allow patching the expected address - otherwise a buggy
+            -- script could corrupt the test environment
+            if target ~= utils.addressof(temp_out) then
+                return expect.fail(('attempted to patch invalid address 0x%x: expected 0x%x'):format(target, utils.addressof(temp_out)))
+            end
+            return originalPatchMemory(target, source, length)
+        end
         local mocks = {
             getAddress = mock.func(utils.addressof(temp_out)),
-            patchMemory = mock.func(),
+            patchMemory = mock.observe_func(safePatchMemory),
         }
         if custom_mocks then
             for k, v in pairs(custom_mocks) do
@@ -49,7 +58,7 @@ end
 local function test_patch_successful(expected_value)
     with_patches(function(mocks, temp_out)
         run_startdwarf(tostring(expected_value))
-        -- temp_out doesn't change on success because our stub patchMemory() is a no-op
+        expect.eq(temp_out.value, expected_value)
 
         expect.eq(mocks.getAddress.call_count, 1, 'getAddress was not called')
         expect.table_eq(mocks.getAddress.call_args[1], {'start_dwarf_count'})
@@ -57,7 +66,7 @@ local function test_patch_successful(expected_value)
         expect.eq(mocks.patchMemory.call_count, 1, 'patchMemory was not called')
         expect.eq(mocks.patchMemory.call_args[1][1], utils.addressof(temp_out),
             'patchMemory called with wrong destination')
-        -- skip checking source (arg 2) because it has already been freed
+        -- skip checking source (arg 2) because it has already been freed by the script
         expect.eq(mocks.patchMemory.call_args[1][3], df.sizeof(temp_out),
             'patchMemory called with wrong length')
     end)
