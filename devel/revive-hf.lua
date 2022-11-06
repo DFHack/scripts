12a8cc1de1f4714a62f4d45e55144bc23b11ba60
dikbut <73856869+Tjudge1@users.noreply.github.com>
2022-11-06 18:38:18 -0500

    Update revive-hf.lua
    Removed unnecessary code.

--- devel/revive-hf.lua
+++ devel/revive-hf.lua
@@ -13,8 +13,6 @@ local hf = df.historical_figure.find(target_hf)
 local hf_name = dfhack.df2console(dfhack.TranslateName(hf.name))
 local hf_desc = ('%i: %s (%s)'):format(target_hf, hf_name, dfhack.units.getRaceNameById(hf.race))
 
-hf.old_year = df.global.cur_year
-hf.old_seconds = df.global.cur_year_tick + 1
 hf.died_year = -1
 hf.died_seconds = -1
 print('Revived histfig ' .. hf_desc)
