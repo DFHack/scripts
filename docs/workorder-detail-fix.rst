c1960e0da718bb5f83352fc00fe3d558c751216e
flashy-man <flashy-man@users.noreply.github.com>
2024-01-06 06:31:03 -0800

    add workorder-detail-fix

---
+++ docs/workorder-detail-fix.rst
@@ -0,0 +1,24 @@
+workorder-detail-fix
+=========
+
+.. dfhack-tool::
+    :summary: Fixes a bug with modified work orders creating incorrect jobs.
+    :tags: fort bugfix workorders
+
+Some work order jobs have a bug when their input item details have been modified.
+
+Example 1: a Stud With Iron order, modified to stud a cabinet, instead creates a job to stud any furniture.
+
+Example 2: a Prepare Meal order, modified to use all plant type ingredients, instead creates a job to use any ingredients.
+
+This fix forces these jobs to properly inherit the item details from their work order.
+
+Usage
+-----
+
+``workorder-detail-fix enable``
+    enables the fix
+``workorder-detail-fix disable``
+    disables the fix
+``workorder-detail-fix status``
+    print fix status
---
+++ workorder-detail-fix.lua
@@ -0,0 +1,114 @@
+local script_name = "workorder-detail-fix"
+local eventful = require 'plugins.eventful'
+if not handler_ref then local handler_ref = nil end 
+
+local function get_job_id(match)
+    for val, name in ipairs(df.job_type) do 
+        if name == match then return val end
+    end
+end
+
+-- all jobs with the "any" (-1) type in its default job_items may be a problem
+local offending_jobs = {
+    [get_job_id("EncrustWithGems")] = true,
+    [get_job_id("EncrustWithGlass")] = true,
+    [get_job_id("StudWith")] = true,
+    [get_job_id("PrepareMeal")] = true,
+    [get_job_id("DecorateWith")] = true,
+    [get_job_id("SewImage")] = true,
+    -- list may be incomplete
+}
+
+-- copy order.item fields/flags over to job's job_item
+-- only the essentials: stuff that is editable via gui/job-details
+local function correct_item_details(job_item, order_item)
+    local fields = {'item_type', 'item_subtype', 'mat_type', 'mat_index'}
+    for _, field in pairs(fields) do 
+        job_item[field] = order_item[field] 
+    end
+
+    local flags_names = {'flags1', 'flags2', 'flags3', 'flags4', 'flags5'}
+    for _, flags in pairs(flags_names) do 
+        local order_flags = order_item[flags]
+        if type(order_flags) == "number" then 
+            job_item[flags] = order_flags
+        else -- copy over the flags one by one
+            for o_flag, val in pairs(order_flags) do 
+                job_item[flags][o_flag] = val
+            end
+        end
+    end
+end
+
+-- correct each job as it's initialized 
+-- this is the handler, running after the job is dispatched
+local function enforce_order_details(job)
+    if not job.job_items then return end -- never happens (error here?)
+    local order_id = job.order_id -- only jobs with an ORDER ID 
+    if (order_id == -1) or (order_id == nil) then return end
+
+    -- only jobs with the item type issue. encrusting, sewing, cooking, etc.
+    if not offending_jobs[job.job_type] then return end
+
+    local order = nil -- get the order ref from order id
+    for _, ord in ipairs(df.global.world.manager_orders) do 
+        if ord.id == order_id then order = ord; break end
+    end
+
+    if not order then return end -- oops, no order
+    if not order.items then return end -- no order item details to enforce
+
+    -- copy the item details over when the types don't match
+    for idx, job_item in ipairs(job.job_items) do
+        local order_item = order.items[idx]
+        if not order_item then break end -- never happens (error here?)
+        if job_item.item_type ~= order_item.item_type then
+            -- dfhack's isSuitableItem function will allow the orders we want,
+            -- but disallow insane combinations like meals made of shoes
+            local suitable = dfhack.job.isSuitableItem(
+                job_item, order_item.item_type, order_item.item_subtype )
+            if suitable then 
+                correct_item_details(job_item, order_item)
+            else --[[ error on unsuitable item?]] end
+        end
+    end
+end
+
+local function enable() 
+    print(script_name.." ENABLED")
+    -- set eventful onJobInitiated handler to run every tick (frequency 0)
+    eventful.enableEvent(eventful.eventType.JOB_INITIATED, 0) 
+    eventful.onJobInitiated.workorder_detail_fix = enforce_order_details
+    handler_ref = eventful.onJobInitiated.workorder_detail_fix
+end
+
+local function disable()
+    print(script_name.." DISABLED")
+    eventful.onJobInitiated.workorder_detail_fix = nil
+    handler_ref = nil
+end
+
+local function status()
+    local status = "DISABLED"
+    local handler = eventful.onJobInitiated.workorder_detail_fix
+    if handler ~= nil then
+        -- ensure the handler still matches the one copied back from eventful
+        if handler == handler_ref then 
+            status = "ENABLED"
+        else
+            status = "ERROR: Handler overwritten!"
+            print("why is this here:", handler)
+            print("should be", handler_ref)
+        end
+    end
+    print(script_name.." status: "..status)
+end
+
+args={...}
+
+cmd_table = { ["enable"]=enable, ["disable"]=disable, ["status"]=status }
+
+cmd = cmd_table[args[1]:lower()]
+if cmd then cmd() else
+    print(script_name.." valid cmds: enable, disable, status")
+end
