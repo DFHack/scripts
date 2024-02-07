dc56d6c0a19197257f4683c1db51e7f94932ab86
Magistrum <136035179+MagistrumT-T@users.noreply.github.com>
2024-02-06 23:33:31 -0300

    Create gui\probe
    Creates a tool that will run probe on the position of the mouse and show it in a screen in the right corner of the screen.

---
+++ gui/probe.lua
@@ -0,0 +1,71 @@
+-- Shows the info given by `probe` in a friendly display
+
+local gui = require('gui')
+local guidm = require('gui.dwarfmode')
+local widgets = require('gui.widgets')
+local overlay = require('plugins.overlay')
+
+Probe = defclass(Probe, widgets.Window)
+Probe.ATTRS{
+    frame = {
+        w = 40,
+        h = 45,
+        r = 2,
+        t = 18,
+    },
+    resizable=true,
+    frame_title='Probe',
+}
+
+function Probe:init()
+    self:addviews{
+		widgets.ToggleHotkeyLabel{
+            view_id='lock',
+            frame={t=0, l=0},
+            key='CUSTOM_CTRL_F',
+            label='Lock on tile:',
+            initial_option=false,
+        },
+        widgets.Label{
+            view_id='report',
+            frame={t=2, l=0},
+        },
+    }
+end
+
+function Probe:onRenderBody()
+	if self.subviews.lock:getOptionValue() then return end
+	guidm.setCursorPos(dfhack.gui.getMousePos())
+    local report = dfhack.run_command_silent('probe')
+    self.subviews.report:setText(report)
+    self:updateLayout()
+end
+
+function Probe:onInput(keys)
+    if Probe.super.onInput(self, keys) then
+        return true
+    end
+    if keys._MOUSE_L and not self:getMouseFramePos() then
+        self.subviews.lock:cycle()
+        return true
+    end
+end
+
+ProbeScreen = defclass(ProbeScreen, gui.ZScreenModal)
+ProbeScreen.ATTRS{
+    focus_string='probe-screen',
+}
+
+function ProbeScreen:init()
+    self:addviews{Probe{}}
+end
+
+function ProbeScreen:onDismiss()
+    view = nil
+end
+
+if not dfhack.isMapLoaded() then
+    qerror("This script requires a fortress map to be loaded")
+end
+
+view = view and view:raise() or ProbeScreen {}:show()
