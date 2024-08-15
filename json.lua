7e1d050f9f5251d75fe17d89c0bb1b7aa788a9f7
Robert Holstein <robert.j.holstein@gmail.com>
2024-08-15 01:14:58 -0400

    Working on combine json lists

--- json.lua
+++ json.lua
@@ -42,17 +42,31 @@ local base_offsets = {
 
 -- Utility functions
 local function getFileHandle(filename)
-    local handle, error = io.open(filename, 'w')
+    local handle, error = io.open(filename, 'r+')
+    if not handle and error:match("No such file or directory") then
+        -- If the file doesn't exist, create it
+        handle, error = io.open(filename, 'w+')
+    end
     if not handle then
         qerror("Error opening file: " .. filename .. ". " .. error)
     end
     return handle
 end
 
+local function readExistingData(handle)
+    local content = handle:read("*a")
+    handle:seek("set", 0)  -- Reset file pointer to start
+    if content and #content > 0 then
+        return json.decode(content)
+    end
+    return {}
+end
+
 local function closeFileHandle(handle, data, filename)
+    handle:seek("set", 0)  -- Reset file pointer to start
     handle:write(json.encode(data))
     handle:close()
-    print('\nData overwritten in "' .. 'Dwarf Fortress/' .. filename .. '"')
+    print('\nData appended in "' .. 'Dwarf Fortress/' .. filename .. '"')
 end
 
 local function reformat(str)
@@ -101,9 +115,9 @@ local function getSexString(sex)
     elseif sex == 1 then
       return "male"
     end
-  end
+end
 
-  local function get_skill_rating_name(rating)
+local function get_skill_rating_name(rating)
     local rating_table = {
         [0] = "Dabbling",
         [1] = "Novice",
@@ -140,8 +154,6 @@ local function serialize_skills(unit)
     return skills
 end
 
-
-
 -- Main logic for item and unit processing
 local item = dfhack.gui.getSelectedItem(true)
 local unit = dfhack.gui.getSelectedUnit(true)
@@ -156,158 +168,87 @@ Please select a valid target and try running the script again.]])
     return
 end
 
+local identifier = nil
+
 if item then
     -- Item processing
     local itemRawName = dfhack.items.getDescription(item, 0, true)
     local itemRawDescription = mi.view_sheets.raw_description
     data = {
-        item = item.id,
+        id = item.id,
         name = dfhack.df2utf(itemRawName),
         description = reformat(dfhack.df2utf(itemRawDescription))
     }
     print('Exporting description of the ' .. itemRawName)
-    filename = 'item_' .. item.id .. '.json'
+    filename = 'items.json'
+    identifier = item.id
 elseif unit then
     -- Get data from unit
-    data['id'] = {}
-    data['id'] = unit.id
-    data['name'] = {}
-    data['name'] = dfhack.units.getReadableName(unit)
-    data['age'] = {}
-    data['age'] = df.global.cur_year - unit.birth_year
-    data['sex'] = {}
-    data['sex'] = getSexString(unit.sex)
-    data['profession'] = {}
-    data['profession'] = dfhack.units.getProfessionName(unit)
-    data['skills'] = serialize_skills(unit)
-    data['race'] = {}
-    data['race'] = df.creature_raw.find(unit.race).name[0]
-    data['isCitizen'] = {}
-    data['isCitizen'] = dfhack.units.isCitizen(unit)
-    data['isResident'] = {}
-    data['isResident'] = dfhack.units.isResident(unit)
-    data['isAlive'] = {}
-    data['isAlive'] = dfhack.units.isAlive(unit)
-    data['isDead'] = {}
-    data['isDead'] = dfhack.units.isDead(unit)
-    data['isKilled'] = {}
-    data['isKilled'] = dfhack.units.isKilled(unit)
-    data['isSane'] = {}
-    data['isSane'] = dfhack.units.isSane(unit)
-    data['isCrazed'] = {}
-    data['isCrazed'] = dfhack.units.isCrazed(unit)
-    data['isGhost'] = {}
-    data['isGhost'] = dfhack.units.isGhost(unit)
-    data['isBaby'] = {}
-    data['isBaby'] = dfhack.units.isBaby(unit)
-    data['isChild'] = {}
-    data['isChild'] = dfhack.units.isChild(unit)
-    data['isAdult'] = {}
-    data['isAdult'] = dfhack.units.isAdult(unit)
-    data['isGay'] = {}
-    data['isGay'] = dfhack.units.isGay(unit)
-    data['isNaked'] = {}
-    data['isNaked'] = dfhack.units.isNaked(unit)
-    data['isForest'] = {}
-    data['isForest'] = dfhack.units.isForest(unit)
-    data['isMischievous'] = {}
-    data['isMischievous'] = dfhack.units.isMischievous(unit)
-    data['isOpposedToLife'] = {}
-    data['isOpposedToLife'] = dfhack.units.isOpposedToLife(unit)
-    data['isBloodsucker'] = {}
-    data['isBloodsucker'] = dfhack.units.isBloodsucker(unit)
-    data['isDwarf'] = {}
-    data['isDwarf'] = dfhack.units.isDwarf(unit)
-    data['isMerchant'] = {}
-    data['isMerchant'] = dfhack.units.isMerchant(unit)
-    data['isDiplomat'] = {}
-    data['isDiplomat'] = dfhack.units.isDiplomat(unit)
-    data['isVisitor'] = {}
-    data['isVisitor'] = dfhack.units.isVisitor(unit)
-    data['isInvader'] = {}
-    data['isInvader'] = dfhack.units.isInvader(unit)
-    data['isUndead'] = {}
-    data['isUndead'] = dfhack.units.isUndead(unit)
-    data['isNightCreature'] = {}
-    data['isNightCreature'] = dfhack.units.isNightCreature(unit)
-    data['isSemiMegabeast'] = {}
-    data['isSemiMegabeast'] = dfhack.units.isSemiMegabeast(unit)
-    data['isMegabeast'] = {}
-    data['isMegabeast'] = dfhack.units.isMegabeast(unit)
-    data['isTitan'] = {}
-    data['isTitan'] = dfhack.units.isTitan(unit)
-    data['isForgottenBeast'] = {}
-    data['isForgottenBeast'] = dfhack.units.isForgottenBeast(unit)
-    data['isDemon'] = {}
-    data['isDemon'] = dfhack.units.isDemon(unit)
-    data['isDanger'] = {}
-    data['isDanger'] = dfhack.units.isDanger(unit)
-
-    data['isAnimal'] = {}
-    data['isAnimal'] = dfhack.units.isAnimal(unit)
-    if data['isAnimal'] then
-        data['isAvailableForAdoption'] = {}
-        data['isAvailableForAdoption'] = dfhack.units.isAvailableForAdoption(unit)
-        data['isPet'] = {}
-        data['isPet'] = dfhack.units.isPet(unit)
-        data['isWar'] = {}
-        data['isWar'] = dfhack.units.isWar(unit)
-        data['isTame'] = {}
-        data['isTame'] = dfhack.units.isTame(unit)
-        data['isTamable'] = {}
-        data['isTamable'] = dfhack.units.isTamable(unit)
-        data['isDomesticated'] = {}
-        data['isDomesticated'] = dfhack.units.isDomesticated(unit)
-        data['isTrained'] = {}
-        data['isTrained'] = dfhack.units.isTrained(unit)
-        data['isHunter'] = {}
-        data['isHunter'] = dfhack.units.isHunter(unit)
-        data['isGelded'] = {}
-        data['isGelded'] = dfhack.units.isGelded(unit)
-        data['isEggLayer'] = {}
-        data['isEggLayer'] = dfhack.units.isEggLayer(unit)
-        data['isEggLayerRace'] = {}
-        data['isEggLayerRace'] = dfhack.units.isEggLayerRace(unit)
-        data['isGrazer'] = {}
-        data['isGrazer'] = dfhack.units.isGrazer(unit)
-        data['isMilkable'] = {}
-        data['isMilkable'] = dfhack.units.isMilkable(unit)
-    end
+    data = {
+        id = unit.id,
+        name = dfhack.units.getReadableName(unit),
+        age = df.global.cur_year - unit.birth_year,
+        sex = getSexString(unit.sex),
+        profession = dfhack.units.getProfessionName(unit),
+        -- skills = serialize_skills(unit),
+        race = df.creature_raw.find(unit.race).name[0]
+    }
 
     -- Get data from view_sheets
     local is_big_portrait = unit.portrait_texpos > 0 and true or false
 
-    for _, entry in ipairs({"status", "wounds", "treatment", "history", "description"}) do
-        local x_offset, y = get_offsets(is_big_portrait, entry)
-        clickAndLog(screen, windowSize, x_offset, y, entry, function()
-            return mi.view_sheets.unit_health_raw_str
-        end)
-    end
+    -- for _, entry in ipairs({"status", "wounds", "treatment", "history", "description"}) do
+    --     local x_offset, y = get_offsets(is_big_portrait, entry)
+    --     clickAndLog(screen, windowSize, x_offset, y, entry, function()
+    --         return mi.view_sheets.unit_health_raw_str
+    --     end)
+    -- end
 
-    for _, entry in ipairs({"traits", "values", "preferences", "needs"}) do
-        local x_offset, y = get_offsets(is_big_portrait, entry)
-        clickAndLog(screen, windowSize, x_offset, y, entry, function()
-            return mi.view_sheets.personality_raw_str
-        end)
-    end
+    -- for _, entry in ipairs({"traits", "values", "preferences", "needs"}) do
+    --     local x_offset, y = get_offsets(is_big_portrait, entry)
+    --     clickAndLog(screen, windowSize, x_offset, y, entry, function()
+    --         return mi.view_sheets.personality_raw_str
+    --     end)
+    -- end
 
-    local thoughts_x_offset, thoughts_y = get_offsets(is_big_portrait, 'thoughts')
-    clickAndLog(screen, windowSize, thoughts_x_offset, thoughts_y, 'thoughts', function()
-        return mi.view_sheets.raw_thought_str
-    end)
+    -- local thoughts_x_offset, thoughts_y = get_offsets(is_big_portrait, 'thoughts')
+    -- clickAndLog(screen, windowSize, thoughts_x_offset, thoughts_y, 'thoughts', function()
+    --     return mi.view_sheets.raw_thought_str
+    -- end)
 
-    local memories_x_offset, memories_y = get_offsets(is_big_portrait, 'memories')
-    clickAndLog(screen, windowSize, memories_x_offset, memories_y, 'memories', function()
-        return mi.view_sheets.thoughts_raw_memory_str
-    end)
+    -- local memories_x_offset, memories_y = get_offsets(is_big_portrait, 'memories')
+    -- clickAndLog(screen, windowSize, memories_x_offset, memories_y, 'memories', function()
+    --     return mi.view_sheets.thoughts_raw_memory_str
+    -- end)
 
     local current_thought_x_offset, current_thought_y = get_offsets(is_big_portrait, 'current_thought')
     clickAndLog(screen, windowSize, current_thought_x_offset, current_thought_y, 'current_thought', function()
         return mi.view_sheets.raw_current_thought
     end)
 
-    filename = 'unit_' .. unit.id .. '.json'
+    filename = 'units.json'
+    identifier = unit.id
 end
 
 local log = getFileHandle(filename)
-closeFileHandle(log, data, filename)
+local existingData = readExistingData(log)
+
+local updated = false
+if data.id then
+    for index, entry in ipairs(existingData) do
+        if entry.id == identifier then
+            -- Update the existing entry
+            existingData[index] = data
+            updated = true
+            break
+        end
+    end
+else
+    updated = true
+end
+
+if not updated then
+    table.insert(existingData, data)
+end
+
+closeFileHandle(log, existingData, filename)
