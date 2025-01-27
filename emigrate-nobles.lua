7a8f07ea2f8b81119e2789107d1fff33561767f5
yg-ong <ong.yinggao@gmail.com>
2025-01-28 03:48:36 +0800

    Add logic for finding noble's sites

--- emigrate-nobles.lua
+++ emigrate-nobles.lua
@@ -14,33 +14,89 @@ local options = {
     help = false
 }
 
+fortId=nil
+
 function isNoble(unit)
-    for _, pos in ipairs(dfhack.units.getNoblePositions(unit) or {}) do
-        entity_pos = pos.position
-        if entity_pos.flags.IS_LAW_MAKER then
-            name = dfhack.df2console(dfhack.units.getReadableName(unit))
-            print(name.." is a noble")
+    local nps = dfhack.units.getNoblePositions(unit) or {}
+    local isLawMaker = false
+
+    local noblePos, nobleName
+
+    for _, np in ipairs(nps) do
+        pos = np.position
+        if pos.flags.IS_LAW_MAKER then
+            isLawMaker = true
+            noblePos = np
+            nobleName = dfhack.df2console(dfhack.units.getReadableName(unit))
+            break
         end
     end
+
+    if not isLawMaker then return false end
+
+    civ = noblePos.entity -- lawmakers seem to be all civ-level positions
+    assignments = civ.positions.assignments
+    for _, link in ipairs(civ.site_links) do
+        siteId = link.target
+        posProfId = link.position_profile_id
+        if posProfId < 0 then goto continue end
+
+        assignment = assignments[posProfId]
+        if assignment.id ~= noblePos.assignment.id then goto continue end
+
+        site = df.world_site.find(siteId)
+        siteName = dfhack.translation.translateName(site.name, true, true)
+
+        if siteId == fortId then
+            print(nobleName.." holds a position in this fortress - "..siteName)
+            goto continue
+        end
+
+        print(nobleName.." is lord of "..siteName)
+        ::continue::
+    end
+
+    return isLawMaker
 end
 
 function main()
+    fort = df.historical_entity.find(fortId)
+    fortName = dfhack.translation.translateName(fort.name, true)
+    print("Current fort is "..fortName)
+
     for _, unit in ipairs(dfhack.units.getCitizens()) do
-        if not options.deport or dfhack.units.isDead(unit) then goto continue end
+        if dfhack.units.isDead(unit) then goto continue end
         isNoble(unit)
         ::continue::
     end
 end
 
+function initChecks()
+    if options.help then
+        print(dfhack.script_help())
+        return false
+    end
+
+    if not dfhack.world.isFortressMode() or not dfhack.isMapLoaded() then
+        qerror('needs a loaded fortress map')
+        return false
+    end
+
+    fortId = dfhack.world.GetCurrentSiteId()
+    if fortId == -1 then
+        qerror('could not find current site')
+        return false
+    end
+
+    return true
+end
 
 argparse.processArgsGetopt({...}, {
     {"h", "help", handler=function() options.help = true end},
     {"a", "all", handler=function() options.deport = true end},
 })
 
-if options.help then
-    print(dfhack.script_help())
-    return
-end
+pass = initChecks()
+if not pass then return end
 
 main()
